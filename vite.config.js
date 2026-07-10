a05e9723d74346ecd8a012e21e95c09f09746c66
sysfab <sysfab64@gmail.com>
2026-07-10 19:08:56 +0300

    fix pages

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 const repositoryName = process.env.GITHUB_REPOSITORY?.split("/").at(-1);
 
 export default defineConfig({
-    base: repositoryName ? `/${repositoryName}/` : "/",
+    base: "/",
     plugins: [react()],
     optimizeDeps: {
         exclude: ["@run-slicer/cfr"],
